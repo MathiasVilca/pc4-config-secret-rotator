2a428cf555bdce714f9c866884ec9a3d53d44150
MathiasVilca <randommathias31@gmail.com>
2025-11-27 21:54:55 -0500

    Corregido funcionalidad de rotate_config.py

--- tools/rotate_secret.py
+++ tools/rotate_secret.py
@@ -1,8 +1,7 @@
 import yaml
-import argparse
 import sys
+from uuid import uuid4
 SECRET_PATH="k8s/secret.yaml"
-VALID_LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
 class DoubleQuoted(str):
     """Clase personalizada para obligar a PyYAML a usar comillas dobles"""
@@ -13,30 +12,19 @@ def double_quoted_representer(dumper, data):
 
 yaml.add_representer(DoubleQuoted, double_quoted_representer)
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Herramienta de Rotación de Configuración")
-    
-    parser.add_argument("--api_key", type=str, help="Define el API_KEY")
-    
-    return parser.parse_args()
-
-def new_config(config_dict):
-    args=parse_arguments()
-    #print(args)
-    if args.api_key:
-        config_dict["stringData"]["API_KEY"]=args.api_key
-    pass
+def new_secret(config_dict):
+    new_api_key=str(uuid4())
+    config_dict["stringData"]["API_KEY"]=new_api_key
+    pass   
 
 try:
     with open(SECRET_PATH,mode='r') as f:
         config_dict=yaml.safe_load(f) #Carga configuracion de configmap
-
     print("Secretos cargados correctamente, cargando nuevos secretos...")
-
-    new_config(config_dict)
-    if 'data' in config_dict:
-            for key, value in config_dict['data'].items():
-                config_dict['data'][key] = DoubleQuoted(value)
+    new_secret(config_dict)
+    if 'stringData' in config_dict:
+            for key, value in config_dict['stringData'].items():
+                config_dict['stringData'][key] = DoubleQuoted(value)
     with open(SECRET_PATH,mode='w') as f:
         yaml.dump(config_dict,f,default_flow_style=False, sort_keys=False)
     
